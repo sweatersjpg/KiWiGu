3a776371c44c2dc31966ea23f39c4b36d5ff022e
Sam Cameron <sweatersjpg@gmail.com>
2023-10-17 00:25:56 -0400

    running / jumping animation

---
+++ kiwigu/Assets/sweatersFPS/sShoot/Scripts/ViewModel/ViewBob.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewBob : MonoBehaviour
+{
+    float time = 0;
+
+    public float timeScale;
+    public Vector3 delta;
+
+    public Vector3 velocity;
+
+    Vector3 targetPosition;
+
+    int hand = -1;
+
+    sweatersController player;
+
+    public WeaponCameraFX cameraFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.parent.localPosition.x > 0) hand = 1;
+        else hand = -1;
+
+        player = sweatersController.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        velocity = player.velocity;
+
+        if (!PauseSystem.paused) time += Time.deltaTime * new Vector2(velocity.x, velocity.z).magnitude;
+
+        float Y = Mathf.Sin(time * timeScale) * delta.y;
+        float X = Mathf.Sin(time * timeScale) * delta.x;
+        float Z = Mathf.Sin(time * timeScale) * delta.z;
+
+        if (velocity.magnitude > 0.5f) targetPosition = new(-X, Y * hand, Z * hand);
+        else targetPosition = new();
+
+        if (!player.isGrounded) targetPosition = new(0, velocity.y * -0.01f, 0);
+
+        if (Mathf.Abs(transform.parent.localPosition.x) < 0.01) targetPosition = 0.1f * targetPosition.magnitude * targetPosition.normalized;
+
+        transform.localPosition += (targetPosition - transform.localPosition) / 4 * Time.deltaTime * 50;
+    }
+}
