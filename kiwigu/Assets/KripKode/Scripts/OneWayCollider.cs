82046588c250b46c1aed7cd62de562aff5fbe428
Carlo <55769242+KripKode@users.noreply.github.com>
2023-11-01 14:57:01 -0400

    defense wall added (sup dup drip scene)

--- kiwigu/Assets/KripKode/Scripts/OneWayCollider.cs
+++ kiwigu/Assets/KripKode/Scripts/OneWayCollider.cs
@@ -1,32 +1,83 @@
-using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class OneWayCollider : MonoBehaviour
 {
-    private Collider thisCollider;
+    [Header("Configuration")]
+    [SerializeField] private Vector3 entryDirection = Vector3.up;
+    [SerializeField] private bool localDirection = false;
 
-    private void Start()
-    {
-        thisCollider = GetComponent<Collider>();
-    }
+    private BoxCollider mainCollider = null;
+    private BoxCollider collisionCheckTrigger = null;
 
-    private void OnTriggerEnter(Collider other)
+    public Vector3 PassthroughDirection
     {
-        if (IsMovingFromFront(other.transform))
+        get
         {
-            Physics.IgnoreCollision(thisCollider, other, false);
-        }
-        else
-        {
-            Physics.IgnoreCollision(thisCollider, other, true);
+            return localDirection
+                ? transform.TransformDirection(entryDirection.normalized)
+                : entryDirection.normalized;
         }
     }
 
-    private bool IsMovingFromFront(Transform otherTransform)
+    private void Awake()
     {
-        Vector3 direction = otherTransform.position - transform.position;
-        float dotProduct = Vector3.Dot(direction, transform.forward);
+        mainCollider = GetComponent<BoxCollider>();
+        CreateCollisionCheckTrigger();
+    }
 
-        return dotProduct > 0f;
+    private void OnValidate()
+    {
+        mainCollider = GetComponent<BoxCollider>();
+        mainCollider.isTrigger = false;
+    }
+
+    private void CreateCollisionCheckTrigger()
+    {
+        collisionCheckTrigger = gameObject.AddComponent<BoxCollider>();
+        collisionCheckTrigger.size = new Vector3(
+            mainCollider.size.x + 4,
+            mainCollider.size.y,
+            mainCollider.size.z + 2
+        );
+        collisionCheckTrigger.center = mainCollider.center;
+        collisionCheckTrigger.isTrigger = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TryIgnoreCollision(other);
+    }
+
+    public void TryIgnoreCollision(Collider other)
+    {
+        if (Physics.ComputePenetration(
+            collisionCheckTrigger, collisionCheckTrigger.bounds.center, transform.rotation,
+            other, other.bounds.center, other.transform.rotation,
+            out Vector3 collisionDirection, out float penetrationDepth))
+        {
+            float dot = Vector3.Dot(PassthroughDirection, collisionDirection);
+
+            if (dot < 0)
+            {
+                if (penetrationDepth < 0.2f)
+                {
+                    Physics.IgnoreCollision(mainCollider, other, false);
+                }
+            }
+            else
+            {
+                Physics.IgnoreCollision(mainCollider, other, true);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.TransformPoint(mainCollider.center), PassthroughDirection * 2);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.TransformPoint(mainCollider.center), -PassthroughDirection * 2);
     }
 }
