821fbe4002f5782d622111bf8465ee6e56b8e011
Carlo <55769242+KripKode@users.noreply.github.com>
2023-11-16 21:05:07 -0500

    hook reticle progress

--- kiwigu/Assets/sweatersFPS/sHookshot/scripts/MoveHook.cs
+++ kiwigu/Assets/sweatersFPS/sHookshot/scripts/MoveHook.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Windows;
-using static UnityEngine.GraphicsBuffer;
 
 public class MoveHook : MonoBehaviour
 {
