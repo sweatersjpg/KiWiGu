737ac1b6470805a1200f91c6e3e234d167825c1e
soha <saritasou.home@gmail.com>
2023-11-22 14:48:31 -0500

    fix black hole vfx, add process work

--- kiwigu/Assets/sohaoying/Scripts/Billboard.cs
+++ kiwigu/Assets/sohaoying/Scripts/Billboard.cs
@@ -4,6 +4,7 @@ public class Billboard : MonoBehaviour
 {
     void FixedUpdate()
     {
-        transform.forward = Camera.main.transform.forward;
+        //transform.forward = Camera.main.transform.forward;
+        transform.LookAt(Camera.main.transform.position, Vector3.up);
     }
 }
