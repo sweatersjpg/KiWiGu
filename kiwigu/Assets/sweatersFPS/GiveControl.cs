bac86b342e7b6f1092121dc997a9bc183d94bfd5
Sam <sweatersjpg@gmail.com>
2023-12-15 06:11:41 -0500

    end screen and kill volumes

---
+++ kiwigu/Assets/sweatersFPS/GiveControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class GiveControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
