d03521f22f65c7fd767a915abcef767508054d5c
KripKode <55769242+KripKode@users.noreply.github.com>
2024-02-20 15:21:07 -0500

    leap gizmos

--- kiwigu/Assets/KripKode/Scripts/AI Behaviour/Enemies/Rehaul/HellfireEnemy.cs
+++ kiwigu/Assets/KripKode/Scripts/AI Behaviour/Enemies/Rehaul/HellfireEnemy.cs
@@ -69,6 +69,8 @@ public class HellfireEnemy : MonoBehaviour
     [SerializeField] float shootCooldown;
     [Range(0, 0.25f)]
     [SerializeField] private float gunSpread;
+    [Range(1, 10)]
+    [SerializeField] private float splatoodRadius;
     public bool isShooting;
     private GunInfo info;
     private float shootTimer;
@@ -304,7 +306,7 @@ public class HellfireEnemy : MonoBehaviour
 
     public void SplatoodEvent()
     {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 5, LayerMask.GetMask("Player"));
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, splatoodRadius, LayerMask.GetMask("Player"));
 
         foreach (Collider hitCollider in hitColliders)
         {
@@ -713,6 +715,7 @@ public class HellfireEnemy : MonoBehaviour
         DrawColoredSphere(transform.position, marchDistance, Color.blue);
         DrawColoredSphere(transform.position, keepDistance, Color.green);
         DrawColoredSphere(transform.position, wanderRadius, Color.yellow);
+        DrawColoredSphere(transform.position, splatoodRadius, Color.magenta);
     }
 
     private void DrawColoredSphere(Vector3 center, float radius, Color color)
