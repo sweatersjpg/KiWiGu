b6fd0aad135cff241db99f2cf6e3ded394c0b050
Sam <sweatersjpg@gmail.com>
2024-03-27 14:52:42 -0400

    tutorial and lighting changes

---
+++ kiwigu/Assets/sweatersFPS/sFPSController/Scripts/Checkpoints/TransitionTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
